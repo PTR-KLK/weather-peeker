c9127dcb4275756c98aab95bb4bf98cd4792ba61
Piotr Kielak <p.kielak@outlook.com>
2020-12-30 02:53:27 +0100

    display more weather data

--- src/components/info.js
+++ src/components/info.js
@@ -1,17 +1,40 @@
 import React from "react";
 
+const getDate = (seconds) => new Date(seconds * 1000);
+const convertDate = (string) => getDate(string).toDateString();
+const convertTime = (string) => getDate(string).toTimeString().slice(0, 5);
+
 function Info({ data }) {
+  const {
+    name,
+    dt,
+    sys,
+    weather: [weather],
+    main,
+  } = data;
+
   return (
     <>
       <h2>
-        {data.name}, {data.sys.country}
+        {name}, {sys.country}
       </h2>
-      <h3>{data.weather[0].main}</h3>
+      <p>{convertDate(dt)}</p>
+      <section>
+        <p>Sunrise: {convertTime(sys.sunrise)}</p>
+        <p>Sunset: {convertTime(sys.sunset)}</p>
+      </section>
+      <h3>{weather.main}</h3>
       <img
-        src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
-        alt={data.weather[0].description}
+        src={`http://openweathermap.org/img/wn/${weather.icon}@2x.png`}
+        alt={weather.description}
       />
-      <h4>Temperature: {data.main.temp} °C</h4>
+      <h3>{Math.round(main.temp)}°C</h3>
+      <p>
+        Min: {Math.round(main.temp_min)}°C | Max: {Math.round(main.temp_max)}°C
+      </p>
+      <p>Feels like: {main.feels_like}°C</p>
+      <p>Pressure: {main.pressure} hPa</p>
+      <p>Humidity: {main.humidity}%</p>
     </>
   );
 }
